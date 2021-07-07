6aca7d60b1896d225d010fc12a8038feae9665d2
zahar <zaharambrozyak@gmail.com>
2021-07-08 00:37:04 +0300

    Hacker news submissions info

---
+++ hn_article.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+# Зробити виклик через API та зберегти відповідь.
+url = 'https://hacker-news.firebaseio.com/v0/item/19155826.json'
+r = requests.get(url)
+print(f"Status code: {r.status_code}")
+
+# Дослідити структуру даних.
+response_dict = r.json()
+readable_file = 'data/readable_hn_data.json'
+with open(readable_file, 'w') as f:
+     json.dump(response_dict, f, indent=4)
